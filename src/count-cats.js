abc4bf6768d1be64c9c6c92fab26b465248f7243
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-02 20:42:22 +0200

    feat: add solution to "Сосчитай котов!"

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let cat = '^^';
+  let arr = backyard.join(',').split(',')
+  let counts = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === cat) {
+      counts++
+    }
+  }
+ return counts
 };
