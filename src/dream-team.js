a3bb5b8ce6ed1ed201254e98e1d6c958f1f38c97
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-03 18:29:10 +0200

    fix: solution "Команда мечты"

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,23 +3,34 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
   let dreamTeam = [];
   let dreamTeamS = '';
-  members && members.length ? members : false
 
-  // members = members.toString().replace(/\s+/g, '').trim().split(',');
-  for (let i = 0; i < members.length; i++) {
+  if (typeof members === !'string') {
+    return members=false;
+  }
+if (typeof members === 'number') {
+    return members=false;
+  }
+if (members === null) {
+    return members=false;
+    }
+
+  if (members === undefined) {
+    return members = false;
+  }
+  // if (typeof members === 'object') {
+  //   return members = false;
+  // }
+
+  for (let i = 0; members !=null && members !=undefined && members !={'foo': 'bar'} && i < members.length ; i++) {
     if (typeof members[i] === 'string') {
-      membersW = members.toString().replace(/\s+/g, '').trim().split(',');
+      membersW = members.toString().replace(/\s+/g, '').trim().toUpperCase().split(',');
       dreamTeam.push(membersW[i][0]);
       dreamTeam.sort();
-    }
-    else if (typeof members[i] === !'string') {
-      return dreamTeamS = false
     }
 
   }
   dreamTeamS = dreamTeam.toString();
   dreamTeamS = dreamTeamS.split(',').join('');
-  dreamTeamS = dreamTeamS.toUpperCase();
   console.log(dreamTeamS)
   return dreamTeamS || members
 
