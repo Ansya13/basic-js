45c6e7d38191153de1c9232cc957da10c7dfa4b0
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-03 22:28:24 +0200

    feat: solution "Какая пора года"

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(strDate) {
+  if (strDate === undefined) {
+    return `${'Unable to determine the time of year!'}`
+  }
+  let dates = Date.parse(strDate);
+  if (isNaN(dates) || strDate.toString !== Date.prototype.toString)
+  throw new Error("Error");
+
+  let currentDate = new Date(dates);
+
+  let month = currentDate.getMonth();
+   if (month < 2 || month === 11) return 'winter';
+   if (month < 5) return 'spring';
+   if (month < 8) return 'summer';
+
+   return 'autumn';
+}
+
+
+
