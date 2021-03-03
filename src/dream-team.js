ac86850a92f4c3b7101bd212c76e2c37ed9b3f5b
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-03 02:00:00 +0200

    feat: add 7 solution of "Команда мечты"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+  let dreamTeamS = '';
+  members && members.length ? members : false
+
+  // members = members.toString().replace(/\s+/g, '').trim().split(',');
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      membersW = members.toString().replace(/\s+/g, '').trim().split(',');
+      dreamTeam.push(membersW[i][0]);
+      dreamTeam.sort();
+    }
+    else if (typeof members[i] === !'string') {
+      return dreamTeamS = false
+    }
+
+  }
+  dreamTeamS = dreamTeam.toString();
+  dreamTeamS = dreamTeamS.split(',').join('');
+  dreamTeamS = dreamTeamS.toUpperCase();
+  console.log(dreamTeamS)
+  return dreamTeamS || members
+
+    }
+
+
+
+
+
