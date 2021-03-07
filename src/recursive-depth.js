1c81924ceb1702f4d45791a332aee3cd0e6e85bb
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-07 15:28:48 +0200

    feat: add solution "Рекурсивный вычислитель глубины"

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!arr.length) { return 1; }
+    let max = 0;
+    let deepCounter = 1;
+    for (let i = 0; i < arr.length; ++i) {
+      if (Array.isArray(arr[i])) {
+        deepCounter = 1 + this.calculateDepth(arr[i]);
+      }
+      if (deepCounter > max) {
+        max = deepCounter;
+        deepCounter = 1;
+      }
+    }
+    return max;
   }
-};
+}
